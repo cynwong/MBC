99f99be5c3eed2f8df18afb6522ca9ea29e214f3
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-10-24 21:34:43 +1100

    code clean up & change alert type

--- assignment3/scripts/password_generator.js
+++ assignment3/scripts/password_generator.js
@@ -1,73 +1,36 @@
-window.onload = function () {
-    let option;
-    //add values for password lenght form field
-    for (let i = 8; i <= 128; i++) {
-        option = document.createElement("option");
-        option.value = i;
-        option.text = i;
-        document.getElementById("pwdLength").add(option);
-    }
-    //for popover
-    // $('[data-toggle="popover"]').popover();
-    // $('.popover-dismiss').popover({
-    //     trigger: 'focus'
-    // });
+//----------- alert message functions ------------------
+let showAlert = function (alertId) {
+    document.querySelector(`#${alertId}`).style.display = "block";
 }
-//create a function to call when the "Generate Password" button is clicked.
-document.getElementById("btn_generateNow").addEventListener("click", function () {
-    //check at least one character type is selected. 
-    if (!document.getElementById("ckb_lowercase").checked && !document.getElementById("ckb_uppercase").checked && !document.getElementById("ckb_numeric").checked && !document.getElementById("ckb_special").checked) {
-        //if not throw an error. and quit the function
-        document.getElementsByClassName("alert")[0].classList.remove("d-none");
+let hideAlert = function (alertId) {
+    document.querySelector(`#${alertId}`).style.display = "none";
+}
+let closeButtonHandler = function (event) {
+    if (event.target.parentElement.matches("div") && event.target.parentElement.classList.contains("modal-footer")) {
+        //this is modal close button
+        // check lowercase characters checkbox again and remove the alert within the modal
+        document.querySelector("#ckb_lowercase").checked = true; 
+        hideAlert("alertOneChartype");
         return;
     }
-    //if at least one char type is selected. we can proceed. 
-    generator();
+    hideAlert(event.target.parentElement.parentElement.attributes.id.nodeValue);
+}
 
-    //there is a password now. we can copy so remove the error message if there is
-    document.getElementById("alertNoPassword").classList.add("d-none");
-});
-
-//"Copy to Clipboard action"
-document.getElementById("btnCopy2Clipboard").addEventListener("click", function () {
-    if (document.getElementById("passwordDisplay").innerHTML.trim().length === 0) {
-        //if no password, there is nothing to copy. 
-        document.getElementById("alertNoPassword").classList.remove("d-none")
-        return;
-    }
-    //clear any current selection
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-
-    //select the password
-    const range = document.createRange();
-    range.selectNodeContents(document.getElementById("passwordDisplay"));
-    selection.addRange(range);
-
-    //copy
-    document.execCommand('copy');
-});
-
-
-
-
-// function generateMethod3() {
-function generator()
-{
+//------------ Password generator ---------------------
+let generator = function () {
     //declare a var to hold the result
     let newPassword = "";
-    let selectedChartype = [];
 
     //get which chartypes are selected. 
-    let inputChartypes = document.getElementsByTagName("input");
-    for (var i = 0; i < inputChartypes.length; i++) {
-        if (inputChartypes[i].checked) {
+    let selectedChartype = [];
+    for (let i of document.querySelectorAll("input[type='checkbox']")) {
+        if (i.checked) {
             //if checked/selected, then add to our queue
-            selectedChartype.push(inputChartypes[i].value);
+            selectedChartype.push(i.value);
         }
     }
 
-    function getRandomChar() {
+    let getRandomChar = function () {
         //create variable to hold all charset
         let ASCIIcodeRanges = {
             lowercase: { start: 97, end: 122 },
@@ -85,105 +48,86 @@ function generator()
             //special characters has 4 ranges so we need to choose which range again
             let i = Math.floor(Math.random() * ASCIIcodeRanges.special.length);
             rangeStart = ASCIIcodeRanges.special[i].start;
-            rangeEnd = ASCIIcodeRanges.special[i].end+1;
+            rangeEnd = ASCIIcodeRanges.special[i].end + 1;
         } else {
             rangeStart = ASCIIcodeRanges[selectedChartype[typeIndex]].start;
-            rangeEnd = ASCIIcodeRanges[selectedChartype[typeIndex]].end+1;
+            rangeEnd = ASCIIcodeRanges[selectedChartype[typeIndex]].end + 1;
         }
         return (String.fromCharCode(Math.floor(Math.random() * (rangeEnd - rangeStart) + rangeStart)));
     }
-    for (var i = 0; i < parseInt(document.getElementById("pwdLength").value); i++) {
+    for (let i = 0; i < parseInt(document.querySelector("#pwdLength").value); i++) {
         newPassword += getRandomChar();
     }
     //display result in passwordDisplay 
-    document.getElementById("passwordDisplay").innerHTML = newPassword;
+    document.querySelector("#passwordDisplay").innerHTML = newPassword;
 
     //hide the modal now to show the result
     $("#modalSettingform").modal("hide");
-
-
 }
 
-// function generateMethod1() {
-//     //create variable to hold chosen charset
-//     let possibleCharSet = "";
-//     //declare a var to hold the result
-//     let generatedPassword = "";
-//     let length = parseInt(document.getElementById("pwdLength").value);
-//     function addToCharSet(charset){
-//         possibleCharSet += charset;
-//     }
-//     //check the checkboxes for which charset to add to chosen charset variable
-//     if (document.getElementById("ckb_lowercase").checked) {
-//         addToCharSet("abcdefghijklmnopqrstuvwxyz");
-//     }
-//     if (document.getElementById("ckb_uppercase").checked) {
-//         addToCharSet("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-//     }
-//     if (document.getElementById("ckb_numeric").checked) {
-//         addToCharSet("0123456789");
-//     }
-//     if (document.getElementById("ckb_special").checked) {
-//         addToCharSet(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-//     }
-//     console.log(possibleCharSet);
-//     //use for loop and Math.random to grap the random chars and add to result var. 
-//     //note:use #pwdLenght for for-loop terminating condition.
+let generateNowHandler = function () {
+    //check at least one character type is selected. 
+    if (!document.querySelector("#ckb_lowercase").checked && !document.querySelector("#ckb_uppercase").checked && !document.querySelector("#ckb_numeric").checked && !document.querySelector("#ckb_special").checked) {
+        //if not throw an error. and quit the function
+        showAlert("alertOneChartype");
+        return;
+    }
+    //if at least one char type is selected. we can proceed. 
+    generator();
 
-//     for (let i = 0, n = possibleCharSet.length; i < length; i++) {
-//         generatedPassword += possibleCharSet.charAt(Math.floor(Math.random() * n));
-//     }
-//     //display result in passwordDisplay 
-//     document.getElementById("passwordDisplay").innerHTML = generatedPassword;
+    //there is a password now. we can copy so remove the error message if there is
+    hideAlert("alertNoPassword");
+};
 
-//     //hide the modal now to show the result
-//     $("#modalSettingform").modal("hide");
-// }
 
-// function generateMethod2() {
-//     //create variable to hold chosen charset
-//     let possibleCharSet = "";
-//     //declare a var to hold the result
-//     let generatedPassword = "";
-//     let length = parseInt(document.getElementById("pwdLength").value);
 
-//     function addToCharSet(min, max) {
-//         for (let i = min; i <= max; i++) {
-//             possibleCharSet += String.fromCharCode(i);
-//         }
-//     }
+// ---------- Copy to Clipboard functions ------------------
+let copy2ClipboardHandler = function () {
+    if (document.querySelector("#passwordDisplay").innerHTML.trim().length === 0) {
+        //if no password, there is nothing to copy.
+        showAlert("alertNoPassword");
+        return;
+    }
 
-//     //check the checkboxes for which charset to add to chosen charset variable
-//     if (document.getElementById("ckb_lowercase").checked) {
-//         // possibleCharSet += "abcdefghijklmnopqrstuvwxyz";
-//         addToCharSet(97, 122);
-//     }
-//     if (document.getElementById("ckb_uppercase").checked) {
-//         // possibleCharSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//         addToCharSet(65, 90);
-//     }
-//     if (document.getElementById("ckb_numeric").checked) {
-//         // possibleCharSet += "0123456789";
-//         addToCharSet(48, 57);
-//     }
-//     if (document.getElementById("ckb_special").checked) {
-//         // possibleCharSet += " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-//         addToCharSet(32, 47);
-//         addToCharSet(58, 64);
-//         addToCharSet(91, 96);
-//         addToCharSet(123, 126);
-//     }
-//     // console.log(possibleCharSet);
-//     //use for loop and Math.random to grap the random chars and add to result var. 
-//     //note:use #pwdLenght for for-loop terminating condition.
+    //clear any current selection
+    const selection = window.getSelection();
+    selection.removeAllRanges();
 
-//     for (let i = 0, n = possibleCharSet.length; i < length; i++) {
-//         generatedPassword += possibleCharSet.charAt(Math.floor(Math.random() * n));
-//     }
-//     //display result in passwordDisplay 
-//     document.getElementById("passwordDisplay").innerHTML = generatedPassword;
+    //select the password
+    const range = document.createRange();
+    range.selectNodeContents(document.getElementById("passwordDisplay"));
+    selection.addRange(range);
 
-//     //hide the modal now to show the result
-//     $("#modalSettingform").modal("hide");
-// }
+    //copy
+    document.execCommand('copy');
+};
 
+
+
+
+//---------------- Event Listeners -----------------
+//create a function to call when the "Generate Password" button is clicked.
+document.querySelector("#btn_generateNow").addEventListener("click", generateNowHandler);
+
+//"Copy to Clipboard action"
+document.querySelector("#btnCopy2Clipboard").addEventListener("click", copy2ClipboardHandler);
+
+
+//Alert close buttons
+document.querySelector("#alertNoPassword button").addEventListener("click", closeButtonHandler);
+document.querySelector("#alertNoPassword button").addEventListener("click", closeButtonHandler);
+document.querySelector(".modal-footer .grayButton").addEventListener("click", closeButtonHandler);
+
+
+
+window.onload = function () {
+    let option;
+    //add values for password lenght form field
+    for (let i = 8; i <= 128; i++) {
+        option = document.createElement("option");
+        option.value = i;
+        option.text = i;
+        document.getElementById("pwdLength").add(option);
+    }
+
+}
