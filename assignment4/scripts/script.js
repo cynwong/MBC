6a72be823cd7d75afdb01686adcf5e86ead34058
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-10-30 00:39:47 +1100

    quiz handler in progress

--- assignment4/scripts/script.js
+++ assignment4/scripts/script.js
@@ -8,10 +8,18 @@
 // -----------------------------
 
 // for quiz challenge
-const NO_OF_QUESTIONS_PER_SESSION = 5;
-const TIME_LIMIT_FOR_EACH_QUESTION = 15; //seconds
+const NO_OF_QUESTIONS_PER_SESSION = 2;//5;
+const TIME_LIMIT_FOR_EACH_QUESTION =  5;//15; //seconds
 const SESSION_TIME_LIMIT = NO_OF_QUESTIONS_PER_SESSION * TIME_LIMIT_FOR_EACH_QUESTION;
-const PENALTY_FOR_INCORRECT_ANSWER = 10; //seconds.
+const PENALTY_FOR_INCORRECT_ANSWER = 15; //seconds.
+
+let sessionTimeRemaining, questionTimeRemaining;
+let questionCount = 0;
+let incorrectAnswerCount = 0;
+
+let sessionTimer, questionTimer;
+
+
 
 //high scores 
 let highscores = [];
@@ -26,95 +34,195 @@ const LOCALSTORAGE_KEY = "highscores";
 // select DOM element by id pass
 // parameter id: string
 // return DOM element
-let getContainer = function(id){
+let getContainer = function (id) {
     let container;
-    if(id === "header"){
+    if (id === "header") {
         container = document.querySelector("header");
-    }else{
+    } else {
         container = document.getElementById(id);
     }
-    return container; 
-}
+    return container;
+};
 
 // hide the container by the id given
 // parameter id: string
 // return null
-let hideContainer = function(id){
+let hideContainer = function (id) {
     let container = getContainer(id);
     container.classList.add("hidden");
-}
+};
 
 // display the container by the id given
 // parameter id: string
 // return null
-let showContainer = function(id){
+let showContainer = function (id) {
     let container = getContainer(id);
     container.classList.remove("hidden");
-}
+};
 
 // ----- Local Storage  -----
 
 // load highscores from localStorage
-let loadHighscores = function(){
-    if(localStorage[LOCALSTORAGE_KEY]){
+let loadHighscores = function () {
+    if (localStorage[LOCALSTORAGE_KEY]) {
         highscores = JSON.parse(localStorage[LOCALSTORAGE_KEY]);
     }
-}
+};
 
 // store highscores in localStorage
-let saveHighscores = function(){
-    if(highscores.length !== 0){
+let saveHighscores = function () {
+    if (highscores.length !== 0) {
         //only save if highscores is not empty
         localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(highscores));
-    }else if(highscores.length === 0){
+    } else if (highscores.length === 0) {
         //if empty, clear the localStorage.
         clearHighscores();
     }
-}
-let clearHighscores = function(){
+};
+let clearHighscores = function () {
     highscores = [];
-    if(localStorage[LOCALSTORAGE_KEY]){
+    if (localStorage[LOCALSTORAGE_KEY]) {
         //if stored in localStorage 
         //clear the localStorage
         localStorage.removeItem(LOCALSTORAGE_KEY);
     }
-}
+};
 
 
 // ----- Quiz -----
-let startQuiz = function(){
-    console.log("startQuiz");
-    
+/*
+    Start quiz - steps
+    when User click "start quiz" button,....
+    1. hide landing page
+    2. set display time from 0 to Session_time
+    3. create a count-down timer 
+        a. with 1 sec interval 
+        b. reduce timeRemaining by 1 for each second and update displaytime
+        c. if user chose an answer or every 15 second clear the interval
+        d. clear the interval 
+            1. when user chose an answer
+            2. after every 15 second (TIME_LIMIT_FOR_EACH_QUESTION)
+            3. when timeRemaining is 0
+            4. when user answers all questions if questionCount === NO_OF_QUESTIONS_PER_SESSION 
+    4. create a fucntion to render Quiz
+        a. get questions
+        b. use Math.random to choose which question to use
+        c. once we get question index
+            1. get quizQuestion to add question.title
+            2. populate answerChoices with questions.answers(a href with button class)
+        d. add eventListener for click event of answerChoices
+            1. get which answer is chosen. 
+            2. check if answer is correct by comparing with question.answer
+            3. if wrong, incorrectAnswerCount + 1
+            4. render another quiz
+    5. when time is run out or user complete all questions,
+        a. calculate score = timeRemaining - (incorrectAnswerCount * PENALTY_FOR_INCORRECT_ANSWER)
+        b. present the user with the score. resultViewContainer
+
+
+
+*/
+
+let updateDisplayTimeRemaining = function () {
+    document.getElementById("timeRemaining").textContent = sessionTimeRemaining;
 }
 
+let startQuiz = function () {
+    console.log("startQuiz");
+    //make sure time limit is always SESSION_TIME_LIMIT
+    sessionTimeRemaining = SESSION_TIME_LIMIT;
+
+    //TODO this need to be render but how. 
+    renderQuiz();
+
+    sessionTimer = setInterval(function () {
+        //update time limit with this new limit on the page. 
+        updateDisplayTimeRemaining();
+        console.log("sessionTimer")
+
+        if (sessionTimeRemaining === 0) {
+            clearSessionTimer();
+            hideContainer("quizViewContainer");
+            renderResult();
+        }
+
+        sessionTimeRemaining--;
+    }, 1000);
+
+
+
+};
+
+let clearSessionTimer = function () {
+    clearQuestionTimer();
+    clearInterval(sessionTimer);
+}
+let clearQuestionTimer = function () {
+    clearInterval(questionTimer);
+}
 
 // ----- Renderers  -----
 
-let renderQuiz = function(){
-    console.log("renderQuiz");
-    console.log(questions);
+let renderQuiz = function () {
+    //restart question time remaining
+    questionTimeRemaining = TIME_LIMIT_FOR_EACH_QUESTION;
+    console.log("renderQuiz", questionTimeRemaining)
+    renderQuestion();
+    questionTimer = setInterval(function () {
+        if (questionTimeRemaining === 0) {
+            clearQuestionTimer();
+            renderQuiz();
+            return false;
+        }
+        questionTimeRemaining--;
+    }, 1000);
+    //load quiz page
+    showContainer("quizViewContainer");
+};
+
+let renderQuestion = function () {
+    //get the question to ask
+    let question = questions[Math.floor(Math.random() * questions.length)];
+    let answerContainer = document.getElementById("answerChoices");
+    let choiceElement;
+    document.getElementById("quizQuestion").textContent = question.title;
+    answerContainer.textContent = "";
+    question.choices.forEach(function (choice) {
+        choiceElement = document.createElement("a");
+        choiceElement.href = "#";
+        choiceElement.textContent = choice;
+        choiceElement.classList.add("button");
+        choiceElement.classList.add("choice");
+        answerContainer.appendChild(choiceElement);
+    })
+
+    
+}
+let renderResult = function () {
+    console.log("render Result")
+
+    showContainer("resultViewContainer");
 
 }
-
-let renderhighscores = function(){
+let renderhighscores = function () {
     //get the results from localstorage
     loadHighscores();
 
     let listElement = document.getElementById("highscoreList");
     let li;
-    highscores.forEach(function(highscore,index){
+    highscores.forEach(function (highscore, index) {
         li = document.createElement("li");
         //TODO**** insert li contents
         listElement.appendChild(li);
     });
-}
+};
 
 // -----------------------------
 //       Event Listeners 
 // -----------------------------
 
 // -----  #landingViewContainer -----
-document.getElementById("highscores").addEventListener("click",function(event){
+document.getElementById("highscores").addEventListener("click", function (event) {
     //prevent the page from reloading.
     event.preventDefault();
 
@@ -129,28 +237,46 @@ document.getElementById("highscores").addEventListener("click",function(event){
     showContainer("highscoresViewContainer");
 });
 
-document.getElementById("btnStart").addEventListener("click",function(event){
+document.getElementById("btnStart").addEventListener("click", function (event) {
     //prevent the page from reloading.
     event.preventDefault();
-    
+
     //  hide loading page
     hideContainer("landingViewContainer");
 
     //render quiz page
-    startQuiz(); 
+    startQuiz();
 
-    //load quiz page
-    showContainer("quizViewContainer");
+
 
 });
 
+// ----- #quizViewContainer -----
 
+function getQuestionObject(question) {
+
+}
+document.getElementById("answerChoices").addEventListener("click", function (event) {
+    //prevent the page from reloading.
+    event.preventDefault();
+
+    clearInterval(questionTimer);
+
+    let answer = event.target.textContent;
+    console.log(answer);
+    let question = questions.filter(obj => { return obj.title === document.getElementById("quizQuestion").textContent });
+    console.log(question);
+    if (answer.trim() !== question.answer.trim()) {
+        //wrong, so increase the worng count
+        incorrectAnswerCount++;
+    }
+});
 
 // ----- #highscoresViewContainer -----
 
-document.getElementById("btnBack").addEventListener("click",function(event){
+document.getElementById("btnBack").addEventListener("click", function (event) {
     //prevent the page from reloading.
-     event.preventDefault();
+    event.preventDefault();
     //hide highscores Page
     hideContainer("highscoresViewContainer");
     //show header and landing page. 
@@ -158,10 +284,10 @@ document.getElementById("btnBack").addEventListener("click",function(event){
     showContainer("landingViewContainer");
 });
 
-document.getElementById("btnClear").addEventListener("click",function(){
+document.getElementById("btnClear").addEventListener("click", function () {
     //prevent the page from reloading.
-     event.preventDefault();
-     console.log("btnClear clicked")
+    event.preventDefault();
+    console.log("btnClear clicked")
     //TODO ** dummy for now. later test this with actual data. 
     clearHighscores();
 
