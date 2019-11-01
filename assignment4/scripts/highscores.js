cfbf09005ae9f510f56bb3da0f5926f957316a38
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-01 17:04:18 +1100

    highscores class

---
+++ assignment4/scripts/highscores.js
@@ -0,0 +1,33 @@
+
+class Highscores {
+    constructor(storageKey) {
+        this._storageKey = storageKey;
+        this.load();
+    }
+    get highscores () {
+        return this._highscores;
+    }
+
+    set highscores ( highscore ) {
+        if( highscore ) {
+            //only save if there is a high score. 
+            this._highscores.push(highscore);
+            this.save();
+        }
+    }
+
+    load () {
+        this._highscores = []; // default
+        if( localStorage[this._storageKey] ) {
+            this._highscores = JSON.parse( localStorage[this._storageKey] );
+        }
+    }
+
+    save () {
+        localStorage.setItem ( this._storageKey, this._highscores );
+    }
+    clear () {
+        this._highscores = [];
+        localStorage.removeItem ( this._storageKey );
+    }
+}
